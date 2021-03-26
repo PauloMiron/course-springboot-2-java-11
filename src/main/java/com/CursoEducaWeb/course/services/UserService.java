07df2b1d03e23d4c143ceeaaa31dc2e2cbc63dba
Paulo Miron <paulomiron16@gmail.com>
2021-03-26 10:25:46 -0300

    Exception Hanler findbyid

--- src/main/java/com/CursoEducaWeb/course/services/UserService.java
+++ src/main/java/com/CursoEducaWeb/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.CursoEducaWeb.course.services;
 
 import com.CursoEducaWeb.course.entities.User;
 import com.CursoEducaWeb.course.repositories.UserRepository;
+import com.CursoEducaWeb.course.services.Exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
       Optional<User> obj = repository.findById(id) ;
-      return obj.get();
+      return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
